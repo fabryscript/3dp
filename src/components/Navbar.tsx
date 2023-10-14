1e3c8a589d7e0626624e49e5ddb9c47db8a6a0a0
fabry-js <vivaldifabrizio10@gmail.com>
2023-10-14 21:08:28 +0200

    feat: :lipstick: center items on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="fixed left-0 right-0 top-4 flex flex-row justify-between px-6">
-      <Image src={"/images/sig.png"} alt="Signature" width={80} height={80} />
+    <div className="fixed left-0 right-0 top-4 flex flex-row items-center justify-between px-6">
+      <Image src={"/images/sig.png"} alt="Signature" width={70} height={70} />
       <button
         type="button"
         className="flex max-h-14 flex-row items-center justify-center rounded-2xl border border-red-500 px-6 py-3"
