105fac99e58382c131e3ec5aea9223ff20e1149b
fabry-js <vivaldifabrizio10@gmail.com>
2023-10-14 22:41:10 +0200

    feat: :sparkles: add dynamic greeting

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,16 +1,82 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
+const GREETINGS = [
+  "Hello",
+  "你好",
+  "Hola",
+  "नमस्ते",
+  "مرحبا",
+  "হ্যালো",
+  "Olá",
+  "привет",
+  "こんにちは",
+  "ਹੈਲੋ",
+  "Hallo",
+  "Hallo",
+  "你好",
+  "Salut",
+  "హలో",
+  "हॅलो",
+  "Merhaba",
+  "ஹலோ",
+  "Xin chào",
+  "Ciao",
+];
+
+const CHANGE_DURATION = 200;
+
 function Title() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentGreeting, setCurrentGreeting] = useState(GREETINGS[0]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newIndex = (currentIndex + 1) % GREETINGS.length;
+      setCurrentIndex(newIndex);
+      setCurrentGreeting(GREETINGS[newIndex]);
+    }, CHANGE_DURATION);
+    if (currentIndex === GREETINGS.length - 1) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
   return (
-    <motion.h1
+    <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
-      className="text-8xl font-bold text-red-500"
+      className="flex gap-2"
     >
-      Hello
-    </motion.h1>
+      <motion.h1
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+        className="text-8xl font-bold text-red-500"
+      >
+        {currentGreeting}
+      </motion.h1>
+      <motion.span
+        initial={{ rotate: 10, scale: 1.2, opacity: 0 }}
+        animate={{
+          rotate: 0,
+          scale: 1,
+          opacity: 1,
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 500,
+          duration: 3,
+          delay: 4.5, // change duration * greetings duration
+        }}
+        className="text-8xl"
+      >
+        👋🏼
+      </motion.span>
+    </motion.div>
   );
 }
 
