fd5e0ef194283bbd7a4abb3376e8b7d830943e4b
fabry-js <vivaldifabrizio10@gmail.com>
2023-10-14 23:58:09 +0200

    feat: :sparkles: add blurs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { Suspense } from "react";
 import { motion } from "framer-motion";
 import Title from "@/components/Title";
 import Loading from "@/components/Loading";
+import ColorBlur from "@/components/ColorBlur";
 
 export default function Home() {
   return (
@@ -26,36 +27,41 @@ export default function Home() {
               staggerChildren: 0.3,
               delayChildren: 1,
             }}
-            className="col-span-12 flex flex-col gap-4 lg:col-span-6"
+            className="relative col-span-12 flex flex-col gap-4 lg:col-span-6"
           >
             <Title />
             <motion.p
               initial={{ opacity: 0 }}
               animate={{ opacity: 0.7 }}
               transition={{ duration: 1.5 }}
-              className="ml-2 text-3xl"
+              className="z-10 ml-2 text-3xl"
             >
               I&apos;m Fabrizio. A web developer based in Milazzo, Italy 🇮🇹
             </motion.p>
+            <ColorBlur className="top-0 h-28 w-28 bg-red-500 blur-[200px]" />
+            <ColorBlur className="-bottom-[500px] h-[300px] w-[300px] bg-red-500 blur-[400px]" />
           </motion.div>
-          <Suspense fallback={<Loading />}>
-            <Canvas className="col-span-12 max-h-[600px] pt-12 lg:col-span-6">
-              <Model />
-              <PerspectiveCamera makeDefault position={[0, 2.5, 8]} />
-              <ambientLight intensity={2.5} />
-              <pointLight position={[1, 1, 2]} />
-              <pointLight position={[-3, 3, 1]} />
-              <pointLight position={[2, 15, 2]} />
-              <OrbitControls
-                maxPolarAngle={1.3} // disable scrolling on the y axis
-                minPolarAngle={1.3} // disable scrolling on the y axis
-                minDistance={8} // disable wheel scrolling
-                maxDistance={8} // disable wheel scrolling
-                autoRotate
-                autoRotateSpeed={5}
-              />
-            </Canvas>
-          </Suspense>
+          <div className="relative col-span-12 flex h-full w-full lg:col-span-6">
+            <ColorBlur className="right-24 top-48 h-[300px] w-[300px] bg-red-500 blur-[500px]" />
+            <Suspense fallback={<Loading />}>
+              <Canvas className="max-h-[600px] flex-1 pt-12">
+                <Model />
+                <PerspectiveCamera makeDefault position={[0, 2.5, 8]} />
+                <ambientLight intensity={2.5} />
+                <pointLight position={[1, 1, 2]} />
+                <pointLight position={[-3, 3, 1]} />
+                <pointLight position={[2, 15, 2]} />
+                <OrbitControls
+                  maxPolarAngle={1.3} // disable scrolling on the y axis
+                  minPolarAngle={1.3} // disable scrolling on the y axis
+                  minDistance={8} // disable wheel scrolling
+                  maxDistance={8} // disable wheel scrolling
+                  autoRotate
+                  autoRotateSpeed={5}
+                />
+              </Canvas>
+            </Suspense>
+          </div>
         </div>
       </main>
     </Suspense>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import React from "react";
+import ColorBlur from "./ColorBlur";
 
 function Navbar() {
   return (
     <div className="fixed left-0 right-0 top-4 flex flex-row items-center justify-between px-6">
+      <ColorBlur className="bottom-52 left-0 h-[300px] w-[300px] bg-yellow-500 blur-[200px]" />
       <Image src={"/images/sig.png"} alt="Signature" width={70} height={70} />
       <button
         type="button"
